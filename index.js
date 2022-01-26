871274d5406516b721eca31992c6792ad2a2429b
lucca1998byu <dit21003@byui.edu>
2022-01-26 17:18:37 -0300

    1.3.1 correction home link

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-document.querySelector("#w1").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W1/main.html")
+document.querySelector("#w1").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/")
 document.querySelector("#w2").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W2/task2.html")
 document.querySelector("#w3").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W3/task3.html")
 document.querySelector("#w4").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W4/task4.html")
