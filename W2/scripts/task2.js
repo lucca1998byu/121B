82942aaee79243c29cb40753fc9f5a6abea50ef4
lucca1998byu <dit21003@byui.edu>
2022-01-26 17:03:34 -0300

    1.2.3 link to home w1

--- W2/scripts/task2.js
+++ W2/scripts/task2.js
@@ -60,6 +60,7 @@ ffood.pop();
 document.querySelector("#food").textContent= ffood;
 
 
+document.querySelector("#w1").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W1/main.html")
 
 document.querySelector("#w3").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W3/task3.html")
 document.querySelector("#w4").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W4/task4.html")
--- W4/scripts/task4.js
+++ W4/scripts/task4.js
@@ -75,6 +75,7 @@ info.placesLived.forEach((placeLived) => {
     document.querySelector('#places-lived').appendChild(placesLivedLength);
 })
    
+document.querySelector("#w1").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W1/main.html")
 
 document.querySelector("#w2").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W2/task2.html")
 document.querySelector("#w3").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W3/task3.html")
