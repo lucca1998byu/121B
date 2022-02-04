45c960a0f0784b5ca7aaf864c49b8f2c8cbe68bf
lucca1998byu <dit21003@byui.edu>
2022-02-04 02:30:54 -0300

    1.9.9

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,3 @@ year = date.getFullYear();
 document.querySelector("#year").textContent = year;
 
 
-
-document.querySelector("#w1").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/");
-document.querySelector("#w2").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W2/task2.html");
-document.querySelector("#w3").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W3/task3.html");
-document.querySelector("#w4").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W4/task4.html");
-document.querySelector("#w5").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W5/task5.html");
-
--- scripts/task.js
+++ scripts/task.js
@@ -5,3 +5,10 @@ const toggleMenu = () => {
 
 document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
 
+
+document.querySelector("#w1").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/");
+document.querySelector("#w2").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W2/task2.html");
+document.querySelector("#w3").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W3/task3.html");
+document.querySelector("#w4").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W4/task4.html");
+document.querySelector("#w5").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W5/task5.html");
+
