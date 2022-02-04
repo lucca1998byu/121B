4631113592f7ce64321b7bbf68d463d04de55c74
lucca1998byu <dit21003@byui.edu>
2022-02-04 02:10:13 -0300

    1.9.4

--- W5/scripts/task5.js
+++ W5/scripts/task5.js
@@ -226,6 +226,5 @@ document.querySelector("#sortBy").addEventListener('change', sortBy);
 
 document.querySelector("#w1").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/")
 document.querySelector("#w2").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W2/task2.html")
-
 document.querySelector("#w3").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W3/task3.html")
 document.querySelector("#w4").setAttribute("href", "https://lucca1998byu.github.io/CSE121B/W4/task4.html")
